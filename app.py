eb136bde313372fd85e7ab75032469dd42dbacc4
alfiyasheikh890@gmail.com <alfiya890@gmail.com>
2022-04-09 16:13:14 +0530

    heroku push

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route('/add')
+def add():
+    num1 = int(request.args.get('num1'));
+    num2 = int(request.args.get('num2'));
+
+    return f"{num1} + {num2} = {num1 + num2}"
+
+#if __name__ == "__main__":
+    #app.run(debug=True);
+    #app.run(host="0.0.0.0", port=int("1234"), debug=True)
