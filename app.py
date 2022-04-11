a910d489b30487bb639aee2003a6cf8f4c5d216e
alfiyasheikh890@gmail.com <alfiya890@gmail.com>
2022-04-11 17:49:22 +0530

    heroku push

--- app.py
+++ app.py
@@ -1,17 +1,78 @@
+import pymysql
+
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pymysql
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+# To connect MySQL database
+conn = pymysql.connect(host='ba9meuiqlprvfo8hjftf-mysql.services.clever-cloud.com', user='u0qgzhbj9orltmqg', password = "edG28gwwDokXveTcW71w", db='ba9meuiqlprvfo8hjftf')
 
-@app.route('/add')
-def add():
-    num1 = int(request.args.get('num1'));
-    num2 = int(request.args.get('num2'));
+@app.route('/users', methods=['GET'])
+def get_users():
 
-    return f"{num1} + {num2} = {num1 + num2}"
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    cur.execute("select * from users LIMIT 10")
+    output = cur.fetchall()
+
+    print(type(output)); #this will print tuple
+
+    for rec in output:
+        print(rec);
+
+    # To close the connection
+    #conn.close()
+
+    return jsonify(output);
+
+@app.route('/users', methods=['DELETE'])
+def deleteRecord():
+    cur = conn.cursor()
+    id = int(request.args.get('id'));
+
+    query = f"delete from users where id = {id}";
+    #print(query)
+    res = cur.execute(query);
+    conn.commit();
+    print(cur.rowcount, "record(s) deleted")
+
+    return "Record deleted sussesfully"
+
+@app.route('/users', methods=['POST'])
+def insertRecord():
+
+        #get raw json values
+        raw_json = request.get_json();
+        name= raw_json['name'];
+        age= raw_json['age'];
+        city= raw_json['city'];
+
+        sql="INSERT INTO users (id,name,age,city) VALUES (NULL,'"+name+"','"+str(age)+"','"+city+"')";
+        cur= conn.cursor()
+
+        cur.execute(sql);
+        conn.commit()
+        return "Record inserted Succesfully"
+
+@app.route('/users', methods=['PUT'])
+def updateRecord():
+
+        raw_json = request.get_json();
+
+        #print(type(raw_json));
+
+        id = raw_json['id'];
+        name= raw_json['name'];
+        age= raw_json['age'];
+        city= raw_json['city'];
+        sql_update_quary=("UPDATE users SET name = '"+name+"',age = '"+str(age)+"',city = '"+city+"'WHERE id = '"+str(id)+"'");
+        cur= conn.cursor()
+        cur.execute(sql_update_quary);
+        conn.commit()
+        return "Record Updated Sussecfully";
+
 
 if __name__ == "__main__":
     #app.run(debug=True);
